c5e526f3baafb00f60f900acf7c9f182a09a47c5
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2019-07-12 13:56:37 -0700

    launch a sample travis test using shunit2 ...

---
+++ travis_tests/sample_script_tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Demo sample_script.sh tests
+
+testSampleScriptParameters() {
+  # Load sample_script.sh for testing
+  . sample_script.sh
+
+  echo "Executing 3 Asserts..."
+
+  assertTrue 'Check default DEMO_PATH' "[ '${DEMO_PATH}' == '/bin/bash' ]"
+  assertTrue 'Check valid DEMO_INT values' "[ $DEMO_INT -ge 0 -a $DEMO_INT -le 2 ]"
+  assertTrue 'Check that DEMO_ARRAY is not empty' "[ ${#DEMO_ARRAY[@]} -ne 0 ]"
+}
+
+. shunit2-2.1.7/src/shunit2
