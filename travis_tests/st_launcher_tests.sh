e7c944fcaf67e9f9fc6ba2bd271519a85ed61215
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2019-07-12 22:41:37 -0700

    2 passing tests and 1 failing test ... on my machine

--- travis_tests/st_launcher_tests.sh
+++ travis_tests/st_launcher_tests.sh
@@ -4,13 +4,35 @@ if [ -z ${ST_LAUNCHER_HOME+x} ]; then
 	export ST_LAUNCHER_HOME="`pwd`/home"
 fi
 
-testHello() {
+testHello_explicitmage() {
+	# execute without error
+  result=`bin/hello.st admin_gs_350 --`
+	status=$?
+	th_validateHello "testHello_exlicitImage" "$status" "$result"
+}
+
+testHello_defaultImage() {
 	# execute without error
   result=`bin/hello.st`
 	status=$?
-	assertEquals "testHello: exit status" \
+	th_validateHello "testHello_defaultImage" "$status" "$result"
+}
+
+testHello_invalidOption() {
+	# execute without error
+  result=`bin/hello.st -x`
+	status=$?
+	assertEquals "testHello_invalidOption: invalid option error" \
+		'1' "$status"
+}
+
+th_validateHello() {
+	testName="$1"
+	status="$2"
+	result="$3"
+	assertEquals "$testName: exit status" \
 		'0' "$status"
-	assertEquals "testHello: wrong output" \
+	assertEquals "$testName: wrong output" \
 		'hello world' "$result"
 }
 
