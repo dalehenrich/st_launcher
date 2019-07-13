956547efbbfa573544b68a8873c45dff1e6a8a72
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2019-07-12 17:53:52 -0700

    simple hello test development

--- travis_tests/st_launcher_tests.sh
+++ travis_tests/st_launcher_tests.sh
@@ -4,7 +4,12 @@
 
 testHello() {
 	# execute without error
-  bin/hello.st
+  bin/hello.st >${stdoutF} 2>${stderrF}
+	rtrn=$?
+	th_assertTrueWithNoOutput ${rtrn} "${stdoutF}" "${stderrF}"
 }
 
+. travis_tests/shunit2-2.1.7/shunit2_test_helpers
+
 . travis_tests/shunit2-2.1.7/shunit2
+
