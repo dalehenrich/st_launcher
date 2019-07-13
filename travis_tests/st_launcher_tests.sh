59f098b5e19b2f2c243a04333650509ce018e0bd
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2019-07-12 19:15:13 -0700

    hmmmm ... hello.st works in one shell window and not another ... nothing obvious so far ... run shunit2 locally ... no .topazini in the non-working shell window

--- travis_tests/st_launcher_tests.sh
+++ travis_tests/st_launcher_tests.sh
@@ -2,14 +2,27 @@
 
 # Demo sample_script.sh tests
 
+set -x
+
+. travis_tests/shunit2-2.1.7/shunit2_test_helpers
+
+if [ -z ${ST_LAUNCHER_HOME+x} ]; then
+	export ST_LAUNCHER_HOME="`pwd`/home"
+fi
+
 testHello() {
 	# execute without error
-  bin/hello.st >${stdoutF} 2>${stderrF}
+  bin/hello.st admin_gs_350 -V -- >${stdoutF} 2>${stderrF}
 	rtrn=$?
 	th_assertTrueWithNoOutput ${rtrn} "${stdoutF}" "${stderrF}"
 }
 
-. travis_tests/shunit2-2.1.7/shunit2_test_helpers
+oneTimeSetUp() {
+	outputDir="${SHUNIT_TMPDIR}/output"
+  mkdir "${outputDir}"
+  stdoutF="${outputDir}/stdout"
+  stderrF="${outputDir}/stderr"
+}
 
 . travis_tests/shunit2-2.1.7/shunit2
 
