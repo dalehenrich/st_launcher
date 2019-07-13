4556dfb18547839fe96df034b834135836047e6d
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2019-07-12 17:04:02 -0700

    tweak travis and add hello.st call (still expecting failure)

--- travis_tests/st_launcher_tests.sh
+++ travis_tests/st_launcher_tests.sh
@@ -2,15 +2,9 @@
 
 # Demo sample_script.sh tests
 
-testSampleScriptParameters() {
-  # Load sample_script.sh for testing
-  . sample_script.sh
-
-  echo "Executing 3 Asserts..."
-
-  assertTrue 'Check default DEMO_PATH' "[ '${DEMO_PATH}' == '/bin/bash' ]"
-  assertTrue 'Check valid DEMO_INT values' "[ $DEMO_INT -ge 0 -a $DEMO_INT -le 2 ]"
-  assertTrue 'Check that DEMO_ARRAY is not empty' "[ ${#DEMO_ARRAY[@]} -ne 0 ]"
+testHello() {
+	# execute without error
+  bin/hello.st
 }
 
 . travis_tests/shunit2-2.1.7/shunit2
