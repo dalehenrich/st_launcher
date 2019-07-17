8b81e59ba44a78ad01ec3de9a4ffa4621e839a63
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2019-07-17 09:31:21 -0700

    Issue #3: looks like the problem I've been fighting has to do with the readability of the $HOME/.st_launcher files (fixing the error message helped) ... let's see

--- bin/install.sh
+++ bin/install.sh
@@ -29,8 +29,12 @@ pushd $HOME
 	if [ ! -d "$HOME/.st_launcher" ] ; then
 		$TRAVIS_SUDO mkdir $HOME/.st_launcher
 		cd $HOME/.st_launcher
-			$TRAVIS_SUDO curl  -L -O -s -S "https://github.com/dalehenrich/st_launcher/releases/download/$st_launcherVersion/st_launcher_default.env"
- 			$TRAVIS_SUDO curl  -L -O -s -S "https://github.com/dalehenrich/st_launcher/releases/download/$st_launcherVersion/st_launcher_home.ston"
+		$TRAVIS_SUDO curl  -L -O -s -S "https://github.com/dalehenrich/st_launcher/releases/download/$st_launcherVersion/st_launcher_default.env"
+ 		$TRAVIS_SUDO curl  -L -O -s -S "https://github.com/dalehenrich/st_launcher/releases/download/$st_launcherVersion/st_launcher_home.ston"
+		if [ "${TRAVIS_SUDO}x" != "x" ] ; then
+			# need to make the .st_launcher dir readable
+			$TRAVIS_SUDO chmod -R +r $HOME/.st_launcher
+		fi
 	fi
 popd
 pushd home/images/admin_gs_350/snapshots
